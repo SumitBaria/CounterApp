008024be84e942412cfe05364a08e1289f4e737b
SumitBaria <imsumitbaria@gmail.com>
2021-04-11 17:18:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import RemoveIcon from "@material-ui/icons/Remove";
+import AddIcon from "@material-ui/icons/Add";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  const minusCounter = () => {
+    return setCounter(counter - 1);
+  };
+
+  const addCounter = () => {
+    return setCounter(counter + 1);
+  };
+
+  let counterStyle = {
+    backgroundColor: " rgb(0, 89, 255)",
+  };
+
+  if (counter <= 5) {
+    counterStyle = {
+      backgroundColor: "red",
+    };
+  }
+  if (counter >= 100) {
+    counterStyle = {
+      backgroundColor: "green",
+    };
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="AppContainer">
+        <div onClick={minusCounter}>
+          <RemoveIcon fontSize="large" className="Btn " />
+        </div>
+
+        <div className="CounterBox" style={counterStyle}>
+          <span className="Counter">{counter}</span>
+        </div>
+        <div onClick={addCounter}>
+          <AddIcon fontSize="large" className="Btn " />
+        </div>
+      </div>
     </div>
   );
 }
